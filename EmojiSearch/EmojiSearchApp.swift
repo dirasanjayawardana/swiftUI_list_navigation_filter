041e30528b5ab64d49ae46c9d915bcd516004810
Mona Lisa <andreno.juanda11@gmail.com>
2024-04-17 08:54:16 +0700

    Initial Commit

---
+++ EmojiSearch/EmojiSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiSearchApp.swift
+//  EmojiSearch
+//
+//  Created by MACBOOK PRO on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
